7d4889e5952e59b4e5b6fb4bcaea44d993241053
abela <abela@ABELARDOCRUZDAM>
2022-03-24 13:30:29 -0600

    Agregar archivos de proyecto.

---
+++ estructura_alumnos/Program.cs
@@ -0,0 +1,14 @@
+﻿Alumno alu = new Alumno();
+alu.Nombre = "Abelardo Cruz Leos";
+alu.Matricula = 641913;
+alu.Grupo = "B";
+alu.Carrera = "TSU";
+Console.WriteLine("El estudiante es: {0} y su matricula es: {1} ", alu.Nombre, alu.Matricula);
+Console.ReadKey(true);
+struct Alumno
+{
+    public string Nombre;
+    public int Matricula;
+    public string Grupo;
+    public string Carrera;
+}
